d001c011136bc524657ac5748c31892670b7abd8
dmitrein35 <dmitrein35@gmail.com>
2026-03-30 17:12:55 +0300

    main.js

--- js/main.js
+++ js/main.js
@@ -7,31 +7,31 @@ var app = new Vue({
                 id: 1, 
                 title: "Orange Navel", 
                 short_text: "Соковитий сорт з яскравим смаком.", 
-                image: "img/orange1.jpg" 
+                image: "images/orange1.jpg" 
             },
             { 
                 id: 2, 
                 title: "Valencia Orange", 
                 short_text: "Найкращий вибір для свіжого соку.", 
-                image: "img/orange2.jpg" 
+                image: "images/orange2.jpg" 
             },
             { 
                 id: 3, 
                 title: "Blood Orange", 
                 short_text: "Унікальний сорт з червоною м'якоттю.", 
-                image: "img/orange3.jpg" 
+                image: "images/orange3.jpg" 
             },
             { 
                 id: 4, 
                 title: "Cara Cara", 
                 short_text: "Рожевий апельсин з низькою кислотністю.", 
-                image: "img/orange4.jpg" 
+                image: "images/orange4.jpg" 
             },
             { 
                 id: 5, 
                 title: "Seville Orange", 
                 short_text: "Гіркуватий сорт для кращого мармеладу.", 
-                image: "img/orange5.jpg" 
+                image: "images/orange5.jpg" 
             }
         ]
     }
